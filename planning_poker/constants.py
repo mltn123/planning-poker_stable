9eb2c755f3a5c5b68c10d69515c6cf534780235d
Malte N <62905344+mltn123@users.noreply.github.com>
2023-10-09 17:14:54 +0200

    Update constants.py

--- planning_poker/constants.py
+++ planning_poker/constants.py
@@ -12,16 +12,26 @@ FIBONACCI_CHOICES = [
     ('34', '34'),
 ]
 
+HOUR_CHOICES = [
+    ('1', '1'),
+    ('2', '2'),
+    ('4', '4'),
+    ('8', '8'),
+    ('16', '16'),
+    ('24', '24'),
+]
+
+
 #: The value which represents the story's scope being too large.
-TOO_LARGE = 'Too large'
+TOO_LARGE = 'Too large (89)'
 #: The value which represents a voter not being able to estimate a story's scope.
-NO_IDEA = 'No idea'
+NO_IDEA = 'No idea (?)'
 
 #: The options which voters can choose, but are not valid story point options.
 NON_POINT_OPTIONS = [
-    (TOO_LARGE, _('Too large')),
-    (NO_IDEA, _('No idea'))
+    (TOO_LARGE, _('Too large (89)')),
+    (NO_IDEA, _('No idea (?)'))
 ]
 
 #: All the options a voter has when voting.
-ALL_VOTING_OPTIONS = FIBONACCI_CHOICES + NON_POINT_OPTIONS
+ALL_VOTING_OPTIONS = FIBONACCI_CHOICES + NON_POINT_OPTIONS + HOUR_CHOICES
